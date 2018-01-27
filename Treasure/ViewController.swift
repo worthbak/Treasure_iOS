28480c2401a3a48e1fa3487b082b528c0264cc01
Worth Baker <wbaker@housecanary.com>
2018-01-27 18:49:52 -0500

    moved to starting point

--- Treasure/ViewController.swift
+++ Treasure/ViewController.swift
@@ -9,24 +9,12 @@
 import UIKit
 import MapKit
 
-final class ViewController: UIViewController, MKMapViewDelegate {
+final class ViewController: UIViewController {
     
     @IBOutlet var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let coordinate = CLLocationCoordinate2D(latitude: 40.0166, longitude: -105.2817)
-        let annotation = MKPlacemark(coordinate: coordinate)
-        mapView.addAnnotation(annotation)
-        
-        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
-        let region = MKCoordinateRegion(center: coordinate, span: span)
-        mapView.setRegion(region, animated: false)
-    }
-    
-    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        let reuseId = "MKPlacemark-reuse"
-        return mapView.dequeueReusableAnnotationView(withIdentifier: reuseId, for: annotation)
     }
 }
